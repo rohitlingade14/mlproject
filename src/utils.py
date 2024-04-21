9acc02b881b569c0195ee77b8faeac9251e499c9
Rohit <lingaders@gmail.com>
2024-04-21 10:37:21 +0530

    app and template

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import r2_score
 from src.exception import CustomException
@@ -43,6 +44,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params, cv=3):
         
         return report
 
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
